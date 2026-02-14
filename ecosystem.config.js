c8d714c21af8560a708d56fcedff1c8cc6638739
TrxModX21 <trxcode21@gmail.com>
2026-02-14 17:21:14 +0700

    add ecosytem-config file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      name: "server-api",
+      script: "./server/dist/server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3021,
+      },
+    },
+    {
+      name: "client-web",
+      script: "pnpm",
+      args: "start",
+      cwd: "./client",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+    {
+      name: "admin-dashboard",
+      script: "pnpm",
+      args: "start",
+      cwd: "./admin",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+      },
+    },
+  ],
+};
